ce035fd38137e779ba3e2f5385e734c6837f29fc
KiRura <kirura@user.noreply.github.com>
2023-04-08 08:22:15 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
-<!-- <img class="backgroud">
-    "ゆた鯖滅亡.png"
-</img>
+<h1>現在準備中</h1>
+<script>
+    import yutasaba from "$lib/assets/yutasaba.jpg";
+</script>
+<img src={yutasaba}
+alt="ゆた鯖内部分裂にトドメを刺す">
 
-<style (lang="css")>
-    background {
-        
+<style type="text/css">
+    h1 {
+        position: relative;
+        top: 130px;
+        right: 350px;
+        text-align: center;
+        text-shadow: 0px 0px 15px rgba(13, 118, 32, 0.778);
+        color: white;
+        font-size: 100px
     }
-</style> -->
-<h1>Hi!</h1>
+</style>
