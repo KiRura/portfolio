bde1fec05a47d371b6c061ab991b122a48690173
KiRura <kirura@user.noreply.github.com>
2023-04-16 06:56:02 +0900

    なんで背景画像がバグっちゃうんですか！！！

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,40 @@
 <div>
-    <h1>現在準備中</h1>
+    <h1 class="texts">
+        現在準備中
+    </h1>
+    <h2 class="texts">
+        背景画像がバグってるサイトだね！
+    </h2>
+    <h3 class="texts">
+        svelteって難しいね！
+    </h3>
 </div>
 
-<style type="text/css">
+<style>
     div {
-        background-image: url($lib/assets/yutasaba.jpg);
-        background-size: 1800px;
-        background-position: center;
-        /* background-color: rgba(173, 173, 173, 0.751); */
+        background: center / contain no-repeat url($lib/assets/yutasaba.jpg);
+        background-color: rgba(173, 173, 173, 0.751);
         background-blend-mode: lighten;
+        background-color: black;
+        background-size: 120%;
+    }
+    .texts {
+        text-align: center;
+        position: relative;
     }
     h1 {
-        position: relative;
-        top: 130px;
-        right: 350px;
-        text-align: center;
         text-shadow: 0px 0px 15px rgba(13, 118, 32, 0.778);
         color: white;
-        font-size: 100px;
+        font-size: 1000%
+    }
+    h2 {
+        text-shadow: 0px 0px 15px rgba(13, 118, 32, 0.778);
+        color: white;
+        font-size: 500%;
+    }
+    h3 {
+        text-shadow: 0px 0px 15px rgba(13, 118, 32, 0.778);
+        color: white;
+        font-size: 300%;
     }
 </style>
-
-
-<!-- ああああ -->
